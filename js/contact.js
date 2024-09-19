a09a82e6ee50c0a211dc214a64396e00d8dd1bba
samsonking14 <samson2049454@gmail.com>
2024-09-19 16:11:37 +0530

    newedition

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,25 @@
-/*!
- * Webflow: Front-end site library
- * @license MIT
- * Inline scripts may access the api using an async handler:
- *   var Webflow = Webflow || [];
- *   Webflow.push(readyFunction);
- */
+function emailSend() {
+  var userName = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
+  var messageBody =
+    "Name " + userName + "<br/> Phone " + phone + "<br/> Email " + email;
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "host@gmail.com",
+    Password: "Password",
+    To: "reviever@gmail.com",
+    From: "website@gmail.com",
+    Subject: "This is the subject",
+    Body: messageBody,
+  }).then((message) => {
+    if (message == "OK") {
+      swal("Secussful", "You clicked the button!", "success");
+    } else {
+      swal("Error", "You clicked the button!", "error");
+    }
+  });
+}
 
 (() => {
   var rm = Object.create;
